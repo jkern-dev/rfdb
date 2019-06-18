11b59003a3265429b5f6337fb0dfc4889ed4796b
Josh Kern <jkern.dev@gmail.com>
2019-06-18 15:30:40 -0700

    organize answer data for chart js charting

--- frontend/src/components/results/results.jsx
+++ frontend/src/components/results/results.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Bar } from 'react-chartjs';
 
 class Results extends React.Component {
   constructor(props) {
@@ -17,6 +16,7 @@ class Results extends React.Component {
           question: question.data
         })
       });
+
     this.props.fetchQuestionAnswers(this.props.match.params.qid)
       .then(({answers}) => {
         this.setState({
@@ -25,12 +25,48 @@ class Results extends React.Component {
       });
   }
 
-  render() {
+  filterRatings() {
+    let ratings = {};
+    ratings["1"] = this.state.answers.filter((answer) => answer.answer === 1);
+    ratings["2"] = this.state.answers.filter((answer) => answer.answer === 2);
+    ratings["3"] = this.state.answers.filter((answer) => answer.answer === 3);
+    ratings["4"] = this.state.answers.filter((answer) => answer.answer === 4);
+    ratings["5"] = this.state.answers.filter((answer) => answer.answer === 5);
+    ratings["6"] = this.state.answers.filter((answer) => answer.answer === 6);
+    ratings["7"] = this.state.answers.filter((answer) => answer.answer === 7)
+    ratings["8"] = this.state.answers.filter((answer) => answer.answer === 8);
+    ratings["9"] = this.state.answers.filter((answer) => answer.answer === 9);
+    ratings["10"] = this.state.answers.filter((answer) => answer.answer === 10);
+    return ratings;
+  }
 
+  render() {
     if (this.state.question === undefined) return null;
     if (this.state.answers.length === 0) return null;
+    let ratings = this.filterRatings();
+    console.log(ratings);
+    const ratingData = {
+      labels: ['1', '2','3','4','5','6','7','8','9','10'],
+      datasets: [{
+        data: [
+          ratings["1"].length,
+          ratings["2"].length,
+          ratings["3"].length,
+          ratings["4"].length,
+          ratings["5"].length,
+          ratings["6"].length,
+          ratings["7"].length,
+          ratings["8"].length,
+          ratings["9"].length,
+          ratings["10"].length,
+        ]
+      }]
+    };
+    console.log(ratingData);
     return (
-      <h1>{this.state.question.body}</h1>
+      <>
+        <h1>{this.state.question.body}</h1>
+      </>
     );
   }
 }
